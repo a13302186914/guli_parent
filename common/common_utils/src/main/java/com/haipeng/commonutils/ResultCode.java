5c09f9b0b7199c9e9756d85f85a9665881be4cb2
“zhp” <zhp@example.com>
2021-02-07 18:43:38 +0800

    添加了头像上传功能第一次

--- common/common_utils/src/main/java/com/haipeng/commonutils/ResultCode.java
+++ common/common_utils/src/main/java/com/haipeng/commonutils/ResultCode.java
@@ -3,7 +3,7 @@ package com.haipeng.commonutils;
 import io.swagger.models.auth.In;
 
 public interface ResultCode {
-    public static Integer SUCCESS=20000;//成功
+    public static Integer SUCCESS=20000;//成功 
     public static Integer ERROR=20001;//失败
 
 
